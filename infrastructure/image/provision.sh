8dccfcc01be3362d219c11ce2f001e43ad258c4d
Robert D French <robert@robertdfrench.me>
2019-05-26 17:03:37 -0400

    Include dehydrate when provisioning

--- infrastructure/image/provision.sh
+++ infrastructure/image/provision.sh
@@ -8,3 +8,7 @@ svcadm enable -r svc:/pkgsrc/apache:default
 
 pkgin -y install postgresql11-server
 svcadm enable -r svc:/pkgsrc/postgresql:default
+
+pkgin -y install dehydrated
+mkdir /opt/local/share/httpd/htdocs/dehydrated
+echo "WELLKNOWN=\"/opt/local/share/httpd/htdocs/dehydrated\"" >> /opt/local/etc/dehydrated/config
